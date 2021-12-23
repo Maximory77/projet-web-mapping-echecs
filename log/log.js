70d872caacf4d82b52e6dddf86561336b850eac2
Florian Bueno <flob2000@hotmail.fr>
2021-12-23 22:03:53 +0100

    FB : authentification+register ok

--- log/log.js
+++ log/log.js
@@ -4,8 +4,11 @@ let pseudo_login = document.getElementById('pseudo_login');
 let mdp_login = document.getElementById('mdp_login');
 let bouton_login = document.getElementById('bouton_login');
 
-let erreur = document.getElementById('erreur');
+let erreur_login = document.getElementById('erreur_login');
+let erreur_regis = document.getElementById('erreur_regis');
 
+let pagesuivante_login = document.getElementById('pagesuivante_login');
+let pagesuivante_regis = document.getElementById('pagesuivante_regis');
 
 var bd_auth;
 var bd_regis;
@@ -25,8 +28,6 @@ bouton_regis.addEventListener('click', () => {
   let pseudo_regis = document.getElementById('pseudo_regis');
   let mdp_regis = document.getElementById('mdp_regis');
   let mail_regis = document.getElementById('mail_regis');
-  console.log("la création du compte commence");
-  console.log("ce compte s'appelle "+pseudo_regis.value);
   // Fonction permettant de créer un compte à partir d'une adresse mail
   // On y associe un pseudo et un mot de passe
   fetch('../log/auth.json.php')
@@ -36,21 +37,21 @@ bouton_regis.addEventListener('click', () => {
     for (let i=0; i<bd_regis.length; i++){
       if(pseudo_regis.value == bd_regis[i].nom){
         // Chaque pseudo est unique, il ne doit pas avoir été déjà utilisé
-        erreur.innerHTML = "<p>Ce pseudo est déjà utilisé</p>";
+        erreur_regis.innerHTML = "<p>Ce pseudo est déjà utilisé</p>";
         break;
       }
       else if (mail_regis.value == bd_regis[i].mail){
         // A chaque adresse mail n'est associé qu'un seul compte
-        erreur.innerHTML = "<p>Un compte est déjà associé à cette adresse</p>";
+        erreur_regis.innerHTML = "<p>Un compte est déjà associé à cette adresse</p>";
         break;
       }
       else if(mdp_regis.value.length<3){
         // Un mot de passe doit être de taille supérieure ou égale à 3 caractères
-        erreur.innerHTML = "<p>Veuillez choisir un mot de passe de 3 caractères ou plus</p>";
+        erreur_regis.innerHTML = "<p>Veuillez choisir un mot de passe de 3 caractères ou plus</p>";
         break;
       }
       else {
-        if (i == bd_regis.length-1){
+        if (i == bd_regis.length-1 && mail_regis.value != bd_regis[i].mail && pseudo_regis.value != bd_regis[i].nom){
           // Si la base de donnée entière a été parcourue sans être interrompue par une des conditions ci-dessus
           // On crée l'entrée dans la base de données, la personne est connectée et on passe sur la page de choix des parties
           var data = 'nom='+pseudo_regis.value+'&mdp='+mdp_regis.value+'&mail='+mail_regis.value;
@@ -64,6 +65,11 @@ bouton_regis.addEventListener('click', () => {
           .then(r => r.text())
           .then(r => {
             bd_regis=r;
+            // On passe sur la page suivante
+            pseudo_regis.style.display = "none";
+            mdp_regis.style.display = "none";
+            mail_regis.style.display = "none";
+            pagesuivante_regis.innerHTML = "<a href='../choixPartie/choixPartie.html'> Cliquer ici pour accèder à la page de choix des parties</a>";
           })
         }
       }
@@ -77,48 +83,35 @@ bouton_regis.addEventListener('click', () => {
 
 
 
-// bouton_login.addEventListener('click', () => {
+bouton_login.addEventListener('click', () => {
+  let pseudo_login = document.getElementById('pseudo_login');
+  let mdp_login = document.getElementById('mdp_login');
+  let bouton_login = document.getElementById('bouton_login');
+  fetch('../log/auth.json.php')
+  .then(r => r.json())
+  .then(r => {
+    bd_auth = r;
 
-  // let pseudo_login = document.getElementById('pseudo_login');
-  // let mdp_login = document.getElementById('mdp_login');
-  // let bouton_login = document.getElementById('bouton_login');
-  // fetch('../log/auth.json.php')
-  // .then(r => r.json())
-  // .then(r => {
-  // // console.log("blabla");
-  // // $.ajax({
-  // //         url: "../log/auth.json.php",
-  // //         type: "GET",
-  // //         // data: {"nom": pseudo_login},
-  // //         dataType: "json",
-  // //         // async: true,         //asynchrone, précision pour certain navigateurs (pas ceux qui pilotent les navires hein)
-  // //         success: function(data,status){
-  //   bd_auth = r;
-  //   console.log(bd_auth);
-  // })
-// });
-  // On regarde dans la base de données si le pseudo et le mot de passe donnés sont dans la base de données
-//   for (let i=0; i<bd_auth.length; i++){
-//     if(pseudo_login == bd_auth[i].pseudo && mdp_login == bd_auth[i].mdp){
-//       //L'authentification a fonctionné, on passe à la page de choix des parties
-//       console.log("OK");
-//     }
-//
-//     //
-//     //
-//     // if(i==bd_auth.length-1 && pseudo_login != bd_auth[i].pseudo){
-//     //   // Pour s'authentifier, le pseudo doit exister
-//     //   console.log("Ce pseudo n'existe pas");
-//     //   alert("Ce pseudo n'existe pas");
-//     // }
-//     // else if(pseudo_login == bd_auth[i].pseudo && mdp_login != bd_auth[i].mdp){
-//     //   // Pour s'authentifier, on vérifie si le mot de passe est associé à ce pseudo
-//     //   console.log("Mot de passe erroné");
-//     //   alert("Mot de passe erroné");
-//     // }
-//     // else if(pseudo_login == bd_auth[i].pseudo && mdp_login == bd_auth[i].mdp){
-//     //   //L'authentification a fonctionné, on passe à la page de choix des parties
-//     //   console.log("OK");
-//     // }
-//   }
-// });
+    // On regarde dans la base de données si le pseudo et le mot de passe donnés sont dans la base de données
+    for (let i=0; i<bd_auth.length; i++){
+      if(pseudo_login.value == bd_auth[i].nom && mdp_login.value == bd_auth[i].mdp){
+        //L'authentification a fonctionné, on modifie l'affichage et on passe à la page de choix des parties
+        pseudo_login.style.display = "none";
+        mdp_login.style.display = "none";
+        bouton_login.style.display = "none";
+        pagesuivante_login.innerHTML = "<a href='../choixPartie/choixPartie.html'> Cliquer ici pour accèder à la page de choix des parties</a>";
+        break;
+      }
+      else if (i==bd_auth.length-1 && pseudo_login.value != bd_auth[i].nom) {
+        //Il faut que le pseudo soit déjà inscrit
+        erreur_login.innerHTML = "<p>Ce pseudo n'existe pas</p>";
+        break;
+      }
+      else if (pseudo_login.value == bd_auth[i].nom && mdp_login != bd_auth[i].mdp) {
+        //Il faut le bon mot de passe associé au bon compte
+        erreur_login.innerHTML = "<p>Mot de passe erroné</p>";
+        break;
+      }
+    }
+  })
+});
