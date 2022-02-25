b2e4dcca879bcae5ada4b2b3c09495da34334d7d
FougerouseC <96108485+FougerouseC@users.noreply.github.com>
2022-02-25 11:35:44 +0100

    maj du script

--- partie/blanc/partie_blanc.js
+++ partie/blanc/partie_blanc.js
@@ -1,3 +1,5 @@
+//mettre des conditions pour la promotion dans je joue. et surtotu deplacement de la piece . (mettre a jour notre plateau). de meme pour les vues quand on decouvre une promotion
+
 //initialisation des Variables
 var plateau =  document.getElementById('plateau');
 var promo =  document.getElementById('promo');
@@ -12,7 +14,8 @@ var coord_piece_select="";
 var type_piece_select="";
 var case_arrive="";
 var rep_iframe="";
-var bool_promo=false;
+var rep_promo="";
+var bol_promo=0;
 var tab_promotion=["case81", "case82", "case83", "case84", "case85", "case86", "case87","case88"];
 var coups_possible="";
 var a_demander=0;
@@ -20,7 +23,7 @@ var list_vues=[];
 
 var plateau=initialisation_plateau();// la plateau vu par le joeur avec les pieces actuelles
 
-//initialisation des attribut pour les envoies au serveur
+//initialisation des attributs pour les envoies au serveur
 var partie = 10;
 var id_joueur = 'Sarko';
 var cote = 1;
@@ -29,13 +32,37 @@ var trait = 1;
 var coup = 0;
 
 // ajout de Baptiste pour recharger et ne pas toujours retourner dans la BDD
-recharger_boutton = document.getElementById('recharger')
+recharger_boutton = document.getElementById('recharger');
 recharger_boutton.addEventListener('click',function() {
   fetch('../recharger.php?partie=' + partie)
   .then(r => {
-    console.log(r)
+    console.log(r);
   })
 })
+//Fin ajout Baptiste
+
+abandonner_boutton = document.getElementById('abandonner');
+abandonner_boutton.addEventListener('click',abandonner);
+
+function abandonner() {
+  //fetch permettant d'envoyer un abandon
+
+  fetch('../nul_abandon.json.php?partie=' + partie + '&id_joueur="' + id_joueur +
+        '"&cote=' + cote + "&abandon=" + 1)
+  .then(r => r.json())
+  .then(r => {
+    console.log(r);
+    if (r["requete"]=='ok'){
+      console.log("coucou, tu as abandonné");
+      defaite();
+
+    }else{
+      console.log("la demande a été refusé");
+    }
+
+  })
+}
+
 
 // FONCTIONS UTILES
 //permet de remettre les cases de la bonne couleur des cases surlignées
@@ -78,10 +105,42 @@ function verification() {
   if(promotion.parentNode != container) {
     container.appendChild(promo);
     clearInterval(verif);
-    rep_promo=rep_iframe.toLowerCase()+"_blanc";
-    $(`#${case_arrive}`).html('<img class="piece" draggable="true" src="../images/'+rep_promo+'.png" alt="'+rep_promo+'">');
+    rep_promo='promo'+rep_iframe.toUpperCase()[0];
+
+    //type_promo=nom_piece(rep_promo,"blanc");
+    //console.log(type_promo);
+
+    //creation de la requete
+    //creation de la structure du coup joue
+    var coup_joue=coord_piece_select[4]+","+coord_piece_select[5]+","+case_arrive[4]+","+case_arrive[5];
+    //recherhce de l'index
+    var index=0;
+    for(let i=0;i<coups_possible.length;i++){
+      let coup_compare=coups_possible[i][0]+","+coups_possible[i][1]+","+coups_possible[i][2]+","+coups_possible[i][3];
+      if (coup_compare==coup_joue){// alors la case de depart et d'arrivee est la même plus qu'a savoir quel pormotion le joueur a choisi
+        if(coups_possible[i].length==5){//alors il s'agit juste d'une promotion sans prendre une piece
+          if(coups_possible[i][4]==rep_promo){
+            index=i;
+            break;
+          }
+        }
+        if (coups_possible[i].length==6){//alors il s'agit juste d'une promotion sans prendre une piece
+          if(coups_possible[i][5]==rep_promo){
+            index=i;
+            break;
+          }
+        }
+
+      }
+    }
     rep_iframe="";
-    bool_promo=true;
+    // on construit la requete
+    coup=index;
+    bol_promo=1;
+    //on lance la requete pour le php
+    joue_j2();
+    var a_demander=0;
+
   }
 }
 
@@ -108,8 +167,10 @@ function joue_uncoupsbis(){
     // on enleve les evenement d'ecoute,; on ne peut plus deplacer les pieces
     $(`.piece`).off('click',remise_couleur);
     $(`.piece`).off('mousedown',dragS);
-    console.log("j'ai supprimer les drag");
+    console.log("en attente de l'autre joueur");
     a_demander=0;
+    // on doit regarder si on a perdu
+    defaite();
   }
 }
 
@@ -128,8 +189,8 @@ function recuperer_les_coups_possible(a_demander2){
 }
 
 
-test=setInterval(joue_uncoupsbis,3000);// fonction qui permet de jouer si on a le trait, elle permet de savoir quels sont les coups possibles et de declancher les evenements d'ecoute sur les cases
-test=setInterval(cr_1_j2,500);//fonction qui regarde toutes les deux seconde à quel tour on est et qui a le trait
+joue=setInterval(joue_uncoupsbis,3000);// fonction qui permet de jouer si on a le trait, elle permet de savoir quels sont les coups possibles et de declancher les evenements d'ecoute sur les cases
+cr=setInterval(cr_1_j2,500);//fonction qui regarde toutes les deux seconde à quel tour on est et qui a le trait
 
 function dragS(e){// fonction d'evenement : elle permet de mettre en surbrillance les cases possible quand on commence en drag and drop
 
@@ -137,11 +198,9 @@ function dragS(e){// fonction d'evenement : elle permet de mettre en surbrillanc
   enleve_surbrillance(surligne);
   surligne=[];
 
-
   //mise à jour des position et du type de la piece bougé
   coord_piece_select = e.target.parentElement["id"];
   type_piece_select = e.currentTarget['alt'];
-
 
   //recuperation des coups popssible en fonction de la piece prise
   for (let i=0;i<coups_possible.length;i++){
@@ -175,57 +234,34 @@ function drop_handler(ev) {// fonction qui permet d'afficher la piece une fois d
   surligne=[];
   affichage_plateau(plateau);//on affiche le plateau
 
-    // on affiche la nouvelle piece
-  $(`#${case_arrive}`).html('<img class="piece" draggable="true" src="../images/'+type_piece_select+'.png" alt="'+type_piece_select+'">');
 
-    // on supprime l'anciene
-  if ((parseInt(coord_piece_select[4])+parseInt(coord_piece_select[5]))%2==0){//la case est noir
-    $(`#${coord_piece_select}`).html('<div class="case_noir_case" id="'+coord_piece_select+'"></div>');
-  }else{//la case est blanche
-    $(`#${coord_piece_select}`).html('<div class="case_blanche_case" id="'+coord_piece_select+'"></div>');
-  }
-
-    // cas d'une Promotion
+    // si il y a une promotion il faut que l'utilisateur choissise la piece avant de lancé la requete
   if ((!(tab_promotion.indexOf(case_arrive)==-1))&&(type_piece_select=="piont_blanc")){// si on est dans la derniere ligne et si c'est un pion
     // on fait appel à l'Iframe fille promotion
+    console.log("je rentre dans la boucle de deplacement");
     promotion.src = '../promotion.html';
-    fctpromotion();
-  }
+    fctpromotion();//la fonction promotion enverra la requete au seveur
 
-  // requete php
+  }else{//si il y a pas de promotion on envoie la requete php
     //on trouve l'index du coup joue dans la liste des coups_possible
-      //creation de la structure du coup joué
+    //creation de la structure du coup joué
     var coup_joue=coord_piece_select[4]+","+coord_piece_select[5]+","+case_arrive[4]+","+case_arrive[5];
-      //recherhce de l'index
+    //recherhce de l'index
     var index=0;
-    console.log(coup_joue);
     for(let i=0;i<coups_possible.length;i++){
       let coup_compare=coups_possible[i][0]+","+coups_possible[i][1]+","+coups_possible[i][2]+","+coups_possible[i][3];
       if (coup_compare==coup_joue){
         index=i;
-        console.log(index);
         break;
       }
     }
-
     // on construit la requete
     coup=index;
-
-    // on met a jour notre cimetiere
-
-    //on met a jour le plateau (deplacement de la piece)
-    plateau=maj_plateau_deplacement_piece(plateau,coup_joue,type_piece_select);
-
     //on lance la requete pour le php
-    console.log("plateau avant l'envoie du fetch");
-    console.log(plateau);
     joue_j2();
-    console.log("plateau juste apres l'envoie du fetch");
-    console.log(plateau);
     var a_demander=0;
+  }
 }
-
-
 
 
 // fonctions utiles pour les coups des joueurs : il s'agit des fetchs pour les envoie au serveur
@@ -236,37 +272,51 @@ function joue_j2(){// fetch permettant d'envoyer le coup que le joueur à décid
         '"&cote=' + cote + "&tour=" + tour + "&trait=" + trait + "&coup=" + coup)
   .then(r => r.json())
   .then(r => {
-    console.log(r)
-    list_vues=r["vues"]//on recupere les vues
-    plateau=maj_plateau_vues(plateau, list_vues)//on met a jour le plateau avec ces nouvelles vues
-    affichage_plateau(plateau);//on affiche le plateau
-    console.log("plateau une fois le fetch terminer");
-    console.log(plateau)
+    console.log(r);
+    if((typeof r['erreur'] == 'undefined')){//on doit verifier que le coup est bien accepté par l'arbitre
+      //on met le deplacement de la piece a jour sur le plateau
+      plateau=maj_plateau_deplacement_piecebis(plateau,r["je_joue"]);
+
+      //on regarde si on n'a pas mis le roi en echec
+      if (!(typeof r['fin'] == 'undefined')){// la partie est fini il y a un gagnant
+        victoire(r['fin']);
+      }
+      list_vues=r["vues"];//on recupere les vues
+      plateau=maj_plateau_vues(plateau, list_vues);//on met a jour le plateau avec ces nouvelles vues
+      affichage_plateau(plateau);//on affiche le plateau
+    }
   })
 }
 function recup_coup_j2(){// fetch permettant de recuperer les coups possibles une fois que l'adversaire au joué :
   fetch('../maj.json.php?partie=' + partie + '&id_joueur="' + id_joueur +
-        '"&cote=' + cote + "&tour=" + tour + "&trait=" + (trait-1))
+        '"&cote=' + cote + "&tour=" + tour + "&trait=" + (trait+1))
   .then(r => r.json())
   .then(r => {
     coups_possible=r["coups"];
+    // console.log("parametre en entree de recup_coup_j2")
+    // console.log(partie);
+    // console.log(id_joueur);
+    // console.log(cote);
+    // console.log(tour);
+    // console.log(trait+1);
+    // console.log("sortie de recup_coup_j2")
     console.log(r);
     // quand une piece apparait directement dans une case visible et qu'on peut la manger
     let il_joue=r["il_joue"];
     let nature=r["nature"];
-    console.log(r["pion pris"]);
     if (r["pion pris"]==0){
       plateau=maj_plateau_deplacement_piece_il_joue(plateau,il_joue, nature);
     }else{
-      console.log("je rentre");
-      plateau=maj_plateau_piece_mangee(plateau,r["pion pris"]);
+      plateau=maj_plateau_piece_mangee(plateau,r["pion pris"],nature);
     }
 
     // quand une piece apparait sur une case visible mais qu'on ne peut pas la manger (un piont devant un autre piont)
-    let vue=r["vues"];
-    if (!(typeof vue[0] == 'undefined')){// la vue existe
-      plateau=maj_plateau_deplacement_piece_il_joue_vue(plateau,vue);
+    let vues=r["vues"];
+    if (!(typeof vues[0] == 'undefined')){// la vue existe
+      plateau=maj_plateau_deplacement_piece_il_joue_vue(plateau,vues);
     }
+
+    //on affiche le plateau avec les nouveaux changements
     affichage_plateau(plateau);
   })
 }
@@ -288,17 +338,31 @@ function cr_1_j2() {// fetch permettant de recuperer le compte rendu du joueur j
     tour=parseInt(r['tour']);
   })
 }
+function defaite(){// fetch permettant de savoir si on a perdu
+  fetch('../maj.json.php?  partie=' + partie + '&id_joueur="' + id_joueur +
+        '"&cote=' + cote + "&tour=" + tour + "&trait=" + trait)
+  .then(r => r.json())
+  .then(r => {
+    if (!(typeof r['fin'] == 'undefined')){// la partie est fini il y a un gagnant
+      victoire(r['fin']);
+    }
+  })
+
+}
 
 //CIMETIERE
 
-// quand on prend une pice ca sera dans je_joue donc dans le fetchs
-//quand je me fais manger ca sera dans plateau_pice_mangee
-function cimetiere(coup_effectue, couleur){//permet de rajouter une piece dans les cimetieres
-  // coup_effectue array de la forme [i_depart,j_depart,i_arrivee,j_arrivee,option] avec dans options dont l'option correspond à la nature de la piece mangé (P,T,F,R,D,C)
+function cimetiere(type_piece_mange, couleur){//permet de rajouter une piece dans les cimetieres
+  // cette fonction est appelee pour mettre à jour les cimetieres des qu'une piece est mangée
+  // quand on prend une piece ca sera dans je_joue donc dans le fetchs
+  // quand je me fais manger ca sera dans plateau_piece_mangee
+  // type_piece_mange est un string de la forme "P","T","C","D","R"
   // couleur est un string representant la couleur de la piece mangée (blanc ou noir)
 
+
+
   //on recupere la nature de la piece mangée
-  type_piece=coup_effectue[4];
+  type_piece=type_piece_mange;
 
   //variable permettant de savoir si le coups speciale est bien une piece qui a été mangée
   piece_mangee=0;
@@ -327,11 +391,16 @@ function cimetiere(coup_effectue, couleur){//permet de rajouter une piece dans l
     type_piece="roi";
     piece_mangee=1;
   }
-  if (piece_mangee==1){// le coups est bien une pice qui a ete mangee
+  if (piece_mangee==1){// le coups est bien une piece qui a ete mangee
     type_piece=type_piece+"_"+couleur;
+    console.log("je mange une piece");
+    var piece_cimetiere = '<img draggable="true" src="../images/'+type_piece+'.png" alt="'+type_piece+'">';
     // on l'ajoute au cimetiere
-    //A MODIFIER en fonction de la couleur pour savoir quel cimetiere
-    //$(`#${cimetiere j1}`).html('<img class="piece" draggable="true" src="../images/'+type_piece+'.png" alt="'+type_piece+'">');
+    if(couleur=="blanc"){// il s'agit d'une piece blanche
+      document.getElementById('cimetiere_j2').innerHTML += piece_cimetiere;
+    }else{// il s'agit d'une piece noire
+      document.getElementById('cimetiere_j1').innerHTML += piece_cimetiere;
+    }
   }
 }
 
@@ -415,28 +484,13 @@ function maj_plateau_vues(plateau, vues){// met a jour le plateau en fonction de
 
     }else{// il y a une piece adverse presente sur la case
       let type_piece=vue[4];
+      if ((!(vue[4]=="pr"))&&(!(vue[4]=="gr"))){
+        // on regarde quel est le type de piece qui est present
+        type_piece=nom_piece(vue[4],"noir");
+        // on ajoute donc la piece sur l'echiquier
+        plateau_maj[coord]=type_piece;
+      }
 
-      // on regarde quel est le type de piece qui est present
-      if (type_piece=='P'){//il s'agit d'un pion
-        type_piece="piont_noir";
-      }
-      if (type_piece=='T'){// il s'agit d'une tour
-        type_piece= "tour_noir";
-      }
-      if (type_piece=='C'){// il s'agit d'un cavalier
-        type_piece="cavalier_noir";
-      }
-      if (type_piece=='F'){// il s'agit d'un fou
-        type_piece="fou_noir";
-      }
-      if (type_piece=='D'){// il s'agit de la dame
-        type_piece="reine_noir";
-      }
-      if (type_piece=='R'){// il s'agit du roi
-        type_piece="roi_noir";
-      }
-      // on ajoute donc la piece sur l'echiquier
-      plateau_maj[coord]=type_piece;
     }
   }
   return plateau_maj;
@@ -490,6 +544,63 @@ function maj_plateau_deplacement_piece(plateau,coup_real,type){
   return plateau;
 }
 
+function maj_plateau_deplacement_piecebis(plateau,je_joue){// deplacement de la piece quand celle si est validée
+  let coord_dep=8*(8-parseInt(je_joue[0]))-1+parseInt(je_joue[1]);//coordonnée de la case dans le plateau car l'origine des reperes est differents
+  let coord_arr=8*(8-parseInt(je_joue[2]))-1+parseInt(je_joue[3]);//coordonnée de la case dans le plateau car l'origine des reperes est differents
+  let type=plateau[coord_dep];// on recupere le type de la piece qui est joué
+
+  //on doit regarder si il s'agit d'un coup special
+  if (je_joue.length>4){//alors il s'agir d'un coup speciale
+    // on a mange une piece adverse
+    if (je_joue[4]=="P" || je_joue[4]=="C" || je_joue[4]=="T" || je_joue[4]=="D" || je_joue[4]=="F" || je_joue[4]=="R"){
+      //on met a jour le cimetiere
+      cimetiere(je_joue[4],"noir");
+    }
+
+    //il s'agit du petit roque ("pr")
+    if(je_joue[4]=="pr"){
+      plateau[63]="v";
+      plateau[62]="roi_blanc";
+      plateau[61]="tour_blanc";
+      plateau[60]="v";
+    }
+    //il s'agit du grand roque ("gr")
+    if (je_joue[4]=="gr"){
+      plateau[60]="v";
+      plateau[59]="tour_blanc";
+      plateau[58]="roi_blanc";
+      plateau[57]="v";
+      plateau[56]="b";
+    }
+    //il s'agit d'une promotion
+    console.log("je suis dans un coups speciale et la valeur de bol_promo est");
+    console.log(bol_promo);
+    if(bol_promo==1){
+      console.log("je rentre dans promo de deplacement");
+      bol_promo=0;
+      if (je_joue.length==5){// il s'agit d'une promo sans prendre de piece adverse
+        console.log("je rentre dans promo sans prise");
+        type=nom_piece(je_joue[4][5],"blanc");
+
+      }else{// il s'agit d'une promo avec une prise de piece adverse
+        console.log("je rentre dans promo avec prise");
+        //on met a jour le cimetiere
+        cimetiere(je_joue[4],"noir");
+        //on change le type de piece
+        type=nom_piece(je_joue[5][5],"blanc");
+      }
+    }
+
+
+  }
+
+  // on met a jour le plateau
+  plateau[coord_dep]="b";
+  plateau[coord_arr]=type;
+  return plateau;
+
+}
+
 function maj_plateau_deplacement_piece_il_joue(plateau, il_joue, nature){
 
   if (il_joue[0]==0 &&(!(il_joue[2]==0))){// alors la piece est apparu
@@ -514,19 +625,60 @@ function maj_plateau_deplacement_piece_il_joue(plateau, il_joue, nature){
   return plateau;
 }
 
-function maj_plateau_deplacement_piece_il_joue_vue(plateau,vue){
+function maj_plateau_deplacement_piece_il_joue_vue(plateau,vues){
 
-  let coord_arr=8*(8-parseInt(vue[0][2]))-1+parseInt(vue[0][3]);
-  let type=nom_piece(vue[0][4],"noir");
-  plateau[coord_arr]=type;
+
+  for (let i=0;i<vues.length;i++){// on met a jour les vues disponibles
+    var vue=vues[i];
+    let coord=8*(8-vue[2])-1+vue[3];//coordonnée de la case dans le plateau car l'origine des reperes est differents
+
+  if (vue.length==4){//la case est vide alors on met dans le plateau la valeur "v" (vue)
+    plateau[coord]="v";//on remplace le brouillard par v
+  }else{// il y a une piece adverse presente sur la case
+    let type_piece=vue[4];
+    if ((!(vue[4]=="pr"))&&(!(vue[4]=="gr"))){
+      // on regarde quel est le type de piece qui est present
+      type_piece=nom_piece(vue[4],"noir");
+      // on ajoute donc la piece sur l'echiquier
+      plateau[coord]=type_piece;
+      }
+    }
+  }
+  return plateau;
+
+}
+
+function maj_plateau_piece_mangee(plateau,il_joue,nature){
+  let piont_ad_i=il_joue["i_dep"];
+  let piont_ad_j=il_joue["j_dep"];
+  let piont_ad_type=nom_piece(nature,"noir");
+  let ligne=il_joue["i"];
+  let col=il_joue["j"];
+  let type_mangee=il_joue["pion"];
+  let coord_piece_mang=8*(8-parseInt(ligne))-1+parseInt(col);
+  let coord_dep=8*(8-parseInt(piont_ad_i))-1+parseInt(piont_ad_j);
+  //si la piece qui a mangé est toujours vsible alors on l'affiche
+  if(!(typeof nature == 'undefined')){
+    plateau[coord_piece_mang]=piont_ad_type;
+  }else{//on met la case de l'ennemie dans le brouillard
+    plateau[coord_piece_mang]="b";
+  }
+  cimetiere(type_mangee,"blanc");
+  plateau[coord_dep]="b";
   return plateau;
 }
 
-function maj_plateau_piece_mangee(plateau,il_joue){
-  //attention le resultat envoyer de il_joue est un string et non un dictionnaire....
-  let coord_dep=8*(8-parseInt(il_joue[5]))-1+parseInt(il_joue[11]);
-  let type_mangee=il_joue[21];
-  plateau[coord_dep]="b";
-  return plateau;
+function victoire(cote){//creer la fin de partie en cas de victoire
+  //cote : numereau du coté gagnant (1 pour les blancs et 2 pour les noirs)
+
+  //suppression des setInterval
+  clearInterval(joue);
+  clearInterval(cr);
+  //affichage du gagnant
+  if(cote==1){
+    console.log("Ce sont les blancs qui ont gagné!!");
+  }else{
+    console.log("Ce sont les noirs qui ont gagné!!");
+  }
 
 }
